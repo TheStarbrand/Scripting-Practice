4e941eca67eacdaf11cc4a257ca39ead4d4db130
Divine Atah <d_atah@u.pacific.edu>
2024-03-28 13:31:13 -0700

    code is buggy, theres a consistent exception

--- get_game_data.py
+++ get_game_data.py
@@ -5,18 +5,40 @@ from subprocess import PIPE, run
 import sys
 
 
-def main(source, target):
+GAME_DIR_PATTERN = 'game'
+
+def find_all_game_paths(source):
+    gamePaths = []
+
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                gamePaths.append(path)
+
+
+        break
+    return gamePaths
+
+
+def main(source):#, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
+    target_path = os.path.join(cwd, target)
+
+    game_paths = find_all_game_paths(source_path)
+    print(game_paths)
 
 
 if __name__ == '__main__':
     args = sys.argv
     # print(args)
-    if len(args) != 3:
-        raise Exception("Invalid number of arguments, you must pass a source and target directory ONLY ")
+    if len(args) != 2:
+        print("Invalid number of arguments, you must pass a source and target directory ONLY ")
+        sys.exit(1)
+        # raise Exception("Invalid number of arguments, you must pass a source and target directory ONLY ")
     
-    source, target = args[1]
+    source, target = args[1:]
     main(source, target)
 
 
